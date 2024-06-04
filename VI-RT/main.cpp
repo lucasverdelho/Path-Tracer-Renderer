90acb0d27e3a12be598feaa1dd1658cf3e70d289
Lucas Verdelho <PG50221@alunos.uminho.pt>
2024-06-04 16:07:18 +0100

    clean up the code

--- VI-RT/main.cpp
+++ VI-RT/main.cpp
@@ -2,8 +2,8 @@
 #include <filesystem>
 #include <vector>
 #include <string>
-#include <iomanip> // for std::setw
-#include <sstream> // for std::ostringstream
+#include <iomanip> 
+#include <sstream> 
 #include "AmbientLight.hpp"
 #include "AmbientShader.hpp"
 #include "AreaLight.hpp"
@@ -29,29 +29,20 @@
 #endif
 
 
-
-
-#include <iomanip> // for std::setw
-#include <sstream> // for std::ostringstream
-
 void save_image(ImagePPM *img, std::string filename = "cornell_box_VI") {
 
-    // Save the image in the rendered_images folder
     std::string save_path = "rendered_images/";
 
     save_path += filename;
 
-    // Append Screen Resolution to the filename
     save_path += "_";
     save_path += std::to_string(img->getHeight());
     save_path += "x";
     save_path += std::to_string(img->getWidth());
 
-    // Get Current day and time
     time_t now = time(0);
     tm *ltm = localtime(&now);
 
-    // Format date and time components into a string
     std::ostringstream date_time;
     date_time << "_"
               << std::setw(4) << std::setfill('0') << (1900 + ltm->tm_year) << "-"   // Year
@@ -60,9 +51,7 @@ void save_image(ImagePPM *img, std::string filename = "cornell_box_VI") {
               << std::setw(2) << std::setfill('0') << ltm->tm_hour << "-"            // Hour
               << std::setw(2) << std::setfill('0') << ltm->tm_min;                   // Minute
 
-    // Append the formatted date and time to the filename
     save_path += date_time.str();
-
     save_path += ".ppm";
 
     if (img->Save(save_path)) {
@@ -75,7 +64,6 @@ void save_image(ImagePPM *img, std::string filename = "cornell_box_VI") {
 
 
 
-// Function that will store inside the input full_path variable the current working directory
 int get_working_directory(std::string &full_path, std::string selected_file = "")
 {
     printf("Full Path: %s\n", full_path.c_str());
@@ -99,13 +87,12 @@ int get_working_directory(std::string &full_path, std::string selected_file = ""
             full_path += std::string("/") + file_path;
         }
 
-        // Output the result
         std::cout << "--------------------------------------------------\n";
         std::cout << "Full Path:\n" << full_path << std::endl;
         std::cout << "--------------------------------------------------\n";
         return 1;
     }
-    else // The input object/scene file is not found
+    else 
     {
         std::cerr << "Error: Unable to locate the object." << std::endl;
         return 0;
@@ -113,7 +100,6 @@ int get_working_directory(std::string &full_path, std::string selected_file = ""
 }
 
 
-// Function to add area lights to the scene
 void add_area_lights(Scene &scene)
 {
     // Add first triangle of square light in the middle
@@ -128,67 +114,64 @@ void add_area_lights(Scene &scene)
     scene.lights.push_back(area_light_1);
     scene.numLights++;
 
-    // Add second triangle of square light in the middle
     AreaLight *area_light_2 = new AreaLight(
-        RGB(0.85, 0.85, 0.85),        // Power of the light (RGB)
+        RGB(0.85, 0.85, 0.85),      
         Point(213, 548, 227),     
         Point(343, 548, 332),       
         Point(213, 548, 332),     
-        Vector(0, -1, 0)               // Normal vector of the area light triangle
+        Vector(0, -1, 0)             
     );
 
     scene.lights.push_back(area_light_2);
     scene.numLights++;
 
+    /*
+    // More Lights for Testing
+    AreaLight *area_light_3 = new AreaLight(
+        RGB(0.85, 0.85, 0.85),     
+        Point(50, 548, 70),     
+        Point(180, 548, 70),       
+        Point(180, 548, 170),     
+        Vector(0, -1, 0)           
+    );
 
-    
-    // // More Lights for Testing
-    // AreaLight *area_light_3 = new AreaLight(
-    //     RGB(0.85, 0.85, 0.85),        // Power of the light (RGB)
-    //     Point(50, 548, 70),     
-    //     Point(180, 548, 70),       
-    //     Point(180, 548, 170),     
-    //     Vector(0, -1, 0)               // Normal vector of the area light triangle
-    // );
+    scene.lights.push_back(area_light_3);
+    scene.numLights++;
 
-    // scene.lights.push_back(area_light_3);
-    // scene.numLights++;
+    AreaLight *area_light_4 = new AreaLight(
+        RGB(0.85, 0.85, 0.85),      
+        Point(50, 548, 70),     
+        Point(180, 548, 170),       
+        Point(50, 548, 170),     
+        Vector(0, -1, 0)           
+    );
 
-    // // Add second triangle of square light in the middle
-    // AreaLight *area_light_4 = new AreaLight(
-    //     RGB(0.85, 0.85, 0.85),        // Power of the light (RGB)
-    //     Point(50, 548, 70),     
-    //     Point(180, 548, 170),       
-    //     Point(50, 548, 170),     
-    //     Vector(0, -1, 0)               // Normal vector of the area light triangle
-    // );
+    scene.lights.push_back(area_light_4);
+    scene.numLights++;
 
-    // scene.lights.push_back(area_light_4);
-    // scene.numLights++;
+        // More Lights for Testing
+    AreaLight *area_light_5 = new AreaLight(
+        RGB(0.85, 0.85, 0.85),      
+        Point(400, 548, 420),     
+        Point(530, 548, 420),       
+        Point(530, 548, 570),     
+        Vector(0, -1, 0)           
+    );
 
-    //     // More Lights for Testing
-    // AreaLight *area_light_5 = new AreaLight(
-    //     RGB(0.85, 0.85, 0.85),        // Power of the light (RGB)
-    //     Point(400, 548, 420),     
-    //     Point(530, 548, 420),       
-    //     Point(530, 548, 570),     
-    //     Vector(0, -1, 0)               // Normal vector of the area light triangle
-    // );
+    scene.lights.push_back(area_light_5);
+    scene.numLights++;
 
-    // scene.lights.push_back(area_light_5);
-    // scene.numLights++;
+    AreaLight *area_light_6 = new AreaLight(
+        RGB(0.85, 0.85, 0.85),      
+        Point(400, 548, 420),     
+        Point(530, 548, 570),       
+        Point(400, 548, 570),     
+        Vector(0, -1, 0)           
+    );
 
-    // // Add second triangle of square light in the middle
-    // AreaLight *area_light_6 = new AreaLight(
-    //     RGB(0.85, 0.85, 0.85),        // Power of the light (RGB)
-    //     Point(400, 548, 420),     
-    //     Point(530, 548, 570),       
-    //     Point(400, 548, 570),     
-    //     Vector(0, -1, 0)               // Normal vector of the area light triangle
-    // );
-
-    // scene.lights.push_back(area_light_6);
-    // scene.numLights++;
+    scene.lights.push_back(area_light_6);
+    scene.numLights++;
+    */
 
 }
 
@@ -220,10 +203,8 @@ void add_point_lights(Scene &scene)
 }
 
 
-// Function to add Ambient lights to the scene
 void add_ambient_lights(Scene &scene)
 {
-    // Add an ambient light to the scene
     AmbientLight ambient(RGB(0.05, 0.05, 0.05));
     scene.lights.push_back(&ambient);
     scene.numLights++;
@@ -231,7 +212,6 @@ void add_ambient_lights(Scene &scene)
 
 
 
-// Function to render the scene
 int render_scene(Scene &scene, 
                  ImagePPM *img, 
                  Camera *cam, 
@@ -239,19 +219,12 @@ int render_scene(Scene &scene,
                  std::string filename = "cornell_box_VI", 
                  bool parallel = true, 
                  int spp = 32, 
-                 int num_threads = 12) {
+                 int num_threads = 12, int W = 1024, int H = 1024) {
 
     printf("Scene: %s\n", filename.c_str());
-
-    // Image resolution
-    const int W = 1024;
-    const int H = 1024;
-
     img = new ImagePPM(W, H);
 
 
-
-    
     // -------------------------------------------------------------------------------------------- //
     // -------------------------/!\ TEMPORARILY HARDCODED CAMERA /!\------------------------------- //
     // -------------------------------------------------------------------------------------------- //
@@ -271,13 +244,6 @@ int render_scene(Scene &scene,
     // -------------------------/!\ TEMPORARILY HARDCODED CAMERA /!\------------------------------- //
     // -------------------------------------------------------------------------------------------- //
 
-
-
-
-
-
-
-    // Get current time
     auto start_cpu = std::clock();
     auto start_real = std::chrono::steady_clock::now();
 
@@ -296,42 +262,19 @@ int render_scene(Scene &scene,
     auto end_cpu = std::clock();
     auto end_real = std::chrono::steady_clock::now();
 
-
-
-
-
-
-
-
-    // Calculate CPU time
     double cpu_time_used = static_cast<double>(end_cpu - start_cpu) / CLOCKS_PER_SEC;
-
-    // Calculate real time with timestamps
     std::chrono::duration<double> real_time_seconds = end_real - start_real;
 
-    // Output results
     std::cout << "CPU Time Used: " << cpu_time_used << " seconds" << std::endl;
     std::cout << "Real Time Elapsed: " << real_time_seconds.count() << " seconds" << std::endl;
 
-
-
-
-
-
     save_image(img, filename);
-
-
 
     return 0;
 }
 
 
 
-
-
-
-
-// Function to load a custom scene
 void load_custom_scene() {
 
     int camera_choice;
@@ -343,7 +286,14 @@ void load_custom_scene() {
     Camera *cam;
     ImagePPM *img;
 
+    // -------------------------------------------------------------------------------------------- //
+    // ------------------------------------Resolution Selection------------------------------------ //
+    // -------------------------------------------------------------------------------------------- //
 
+    int W, H;
+
+    std::cout << "Enter the resolution of the image (W x H):\n";
+    std::cin >> W >> H;
 
     // -------------------------------------------------------------------------------------------- //
     // ------------------------------------Camera Selection---------------------------------------- //
@@ -374,43 +324,31 @@ void load_custom_scene() {
     //         return;
     // }
 
-
-
-
-
-
     // -------------------------------------------------------------------------------------------- //
     // ------------------------------------Shader Selection---------------------------------------- //
     // -------------------------------------------------------------------------------------------- //
 
-
-    // Expor os shaders disponiveis
     std::cout << "\nSelect a shader:\n";
     std::cout << "1. Ambient Shader\n";
     std::cout << "2. Whitted Shader\n";
     std::cout << "3. Distributed Shader\n";
     std::cout << "4. Path Tracer Shader\n";
 
-    // Ler a escolha do utilizador
     std::cin >> shader_choice;
 
 
     RGB background(0.05, 0.05, 0.55);
     switch (shader_choice) {
         case 1:
-            // Ambient Shader
             shd = new AmbientShader(&scene, background);
             break;
         case 2:
-            // Whitted Shader
             shd = new WhittedShader(&scene, background);
             break;
         case 3:
-            // Distributed Shader
             shd = new DistributedShader(&scene, background);
             break;
         case 4:
-            // Path Tracer Shader
             shd = new PathTracerShader(&scene, background);
             break;
         default:
@@ -419,50 +357,39 @@ void load_custom_scene() {
     }
 
 
-
-
-
-
     // -------------------------------------------------------------------------------------------- //
     // ------------------------------------Scene Selection----------------------------------------- //
     // -------------------------------------------------------------------------------------------- //
 
 
 
-
     // Expor as cenas disponiveis (ficheiros .obj na pasta tinyobjloader/models)
     std::cout << "\nSelect a scene:\n";
 
-    // vector to store file names
     std::vector<std::string> file_names;
 
-
     char buffer[FILENAME_MAX];
-
     GetCurrentDir(buffer, sizeof(buffer));
-
     std::string full_path = std::string(buffer) + "/VI-RT/Scene/tinyobjloader/models";
 
-    // For loop to read all the .obj file names in the tinyobjloader/models folder and store them in a vector
+
     for (const auto & entry : std::filesystem::directory_iterator(full_path))
         if (entry.path().extension() == ".obj") {
             file_names.push_back(entry.path().filename().string());
         }
 
-    // Print the names of the files to the user
+
     for (int i = 0; i < file_names.size(); i++) {
         std::cout << i+1 << ". " << file_names[i] << "\n";
     }
 
-    // Ler a escolha do utilizador
+
     std::cin >> scene_choice;
     std::string selected_file = file_names[scene_choice-1];
 
     if (get_working_directory(full_path, selected_file) == 0) 
         return;
 
-
-    // Attempt to load the scene
     if(!scene.Load(full_path))
     {
         std::cout << "Loading the scene failed.\n";
@@ -472,17 +399,13 @@ void load_custom_scene() {
 
     scene.printSummary();
 
-
-    // Add lights to the scene
     add_area_lights(scene);
     // add_point_lights(scene);
-
 
 
     // -------------------------------------------------------------------------------------------- //
     // -------------------------------------Render Scene------------------------------------------- //
     // -------------------------------------------------------------------------------------------- //
-
 
 
     int parallel_choice;
@@ -507,7 +430,6 @@ void load_custom_scene() {
         parallel = false;
     
 
-
     std::cout << "Enter the number of samples per pixel (spp):\n";
     std::cin >> spp;
 
@@ -523,28 +445,21 @@ void load_custom_scene() {
 }
 
 
-// Function to load the default scene
 void load_default_scene() {
 
-    Perspective *cam; // Camera
-    ImagePPM *img;    // Rendered Image
+    Perspective *cam; 
+    ImagePPM *img;   
     Shader *shd;
     Scene scene;
 
-    // Ambient Shader
     RGB background(0.05, 0.05, 0.55);
 
-    // Path Tracer Shader
     shd = new PathTracerShader(&scene, background);
 
-    // Get the current working directory ny storing it in the full_path variable
     std::string full_path;
-
     if (get_working_directory(full_path) == 0) 
         return; 
 
-
-    // Attempt to load the scene
     if(!scene.Load(full_path))
     {
         std::cout << "Loading the scene failed.\n";
@@ -554,13 +469,10 @@ void load_default_scene() {
 
     scene.printSummary();
 
-
-    // Add lights to the scene
     add_area_lights(scene);
     // add_point_lights(scene);
 
     render_scene(scene, img, cam, shd);
-
 
     std::cout << "Program finished successfully.\n";
     std::cout << "--------------------------------------------------\n";
