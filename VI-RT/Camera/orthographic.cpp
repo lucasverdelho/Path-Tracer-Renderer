b7026e7cff95852a5f35d625b30d1b5c5e4afeeb
Lucas Verdelho <PG50221@alunos.uminho.pt>
2024-06-14 20:03:10 +0100

    fixed orthogonal camera

--- VI-RT/Camera/orthographic.cpp
+++ VI-RT/Camera/orthographic.cpp
@@ -1,4 +1,5 @@
 #include "orthographic.hpp"
+#include <stdio.h>
 
 Orthographic::Orthographic(const Point Eye, const Point At, const Vector Up, const int W, const int H) 
     : Eye(Eye), At(At), Up(Up), W(W), H(H)
@@ -25,30 +26,29 @@ Orthographic::Orthographic(const Point Eye, const Point At, const Vector Up, con
     c2w[2][1] = F.Y;
     c2w[2][2] = F.Z;
 
-
-    // Compute the real width and height coordinates of the camera
-    realW = Eye.X + W/2;
-    realH = Eye.Y + H/2;
-
 }
 
 bool Orthographic::GenerateRay(int x, int y, Ray *r, const float *cam_jitter)
 {
-    float xc, yc;
+ 
+    // Orthographic projection ray generation
+    // float sx = (2.0f * (x + 0.5f) / W - 1.0f);  // Normalized device coordinates x
+    // float sy = (2.0f * (y + 0.5f) / H - 1.0f);  // Normalized device coordinates y
 
-    xc = realW - x + cam_jitter[0];
-    yc = realH - y - cam_jitter[1];
+    //no need to normalize, but we need to put them in the right place in the world
+    float sx = -W/2 + x + cam_jitter[0];
+    float sy = H/2 - y + cam_jitter[1];
 
-    Point origin = Point(xc, yc, Eye.Z);
+    // Calculate ray origin
+    Point origin = Eye + sx * Vector(c2w[0][0], c2w[0][1], c2w[0][2]) + sy * Vector(c2w[1][0], c2w[1][1], c2w[1][2]);
+    
+    // printf("Origin: %f %f %f\n", origin.X, origin.Y, origin.Z);
+    // Orthographic camera has a constant direction
+    Vector direction = Vector(c2w[2][0], c2w[2][1], c2w[2][2]);
 
-    // The direction is the same for all rays in an orthographic camera
-    Vector direction = Vector(
-        c2w[2][0],
-        c2w[2][1],
-        c2w[2][2]
-    );
-
+    // Create the ray
     *r = Ray(origin, direction);
 
     return true;
 }
+
